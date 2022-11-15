4b03ed33a1b31e5383b484dc500b5c1de69914c2
DalyChouikh <chouikhdaly215@gmail.com>
2022-11-15 14:49:19 +0100

    Removed sequence generator

--- src/main/java/com/classna/classna/Model/Student.java
+++ src/main/java/com/classna/classna/Model/Student.java
@@ -23,12 +23,7 @@ import javax.persistence.UniqueConstraint;
 )
 public class Student {
     @Id
-    @SequenceGenerator(
-        name = "student_sequence",
-        sequenceName = "student_sequence",
-        allocationSize = 1
-    )
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(
         name = "id",
         updatable = false
